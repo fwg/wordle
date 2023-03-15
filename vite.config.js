74f83085e36054b1d2d51be9f5637c2f8bb80196
Friedemann Altrock <hallo@faltrock.de>
2023-03-15 12:31:44 +0100

    add http-server, gzip compression for assets

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { version } from "./package.json";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle/",
-  plugins: [svelte({
-    preprocess: vitePreprocess()
-  })],
+  base: "/",
+  plugins: [
+    svelte({ preprocess: vitePreprocess() }), 
+    viteCompression(),
+  ],
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        not_found: resolve(__dirname, '404.html'),
+      },
       output: {
         assetFileNames: `[name]-v${version}.[ext]`,
         entryFileNames: `[name]-v${version}.js`,
